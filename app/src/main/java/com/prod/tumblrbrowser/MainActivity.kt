b0cd15f21be515ffb1fce7d8ed22baba11739a5b
Piotr Jaszczurowski <piotrjaszczurowski87@gmail.com>
2020-01-07 20:10:56 +0100

    Initial commit

---
+++ app/src/main/java/com/prod/tumblrbrowser/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.prod.tumblrbrowser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by Piotr Jaszczurowski on 07.01.2020
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
